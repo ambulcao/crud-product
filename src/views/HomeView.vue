d383e99c8f674f413947f18f1cfba267859eda39
Alexandre Marques Bulcao <ambulcao@gmail.com>
2024-08-23 23:20:48 +0100

    chore: reconfiguração do projeto
    chore: reconfiguração do projeto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="title">Latest Post</h1>
   </main>
 </template>
