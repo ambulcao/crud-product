64e2de8ad1203ccbbb81c3cb22e7edcf2377017b
Alexandre Marques Bulcao <ambulcao@gmail.com>
2024-08-24 14:30:13 +0100

    chore: criando estrutura backend
    chore: criando estrutura backend para base de dados, policies, models, factories, create_table, migrates e seeders.

---
+++ database/migrations/2024_08_24_122245_create_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('descricao');
+            $table->decimal('preco', 10, 2); 
+            $table->date('data_de_validade');
+            $table->string('imagem')->nullable();
+            $table->unsignedBigInteger('categoria_id'); 
+            $table->foreign('categoria_id')->references('id')->on('categorias'); 
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
