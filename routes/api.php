5f6b17a0bea83616a359ae99295396c154b44550
Alexandre Marques Bulcao <ambulcao@gmail.com>
2024-08-24 13:13:09 +0100

    chore: criação de controllers
    chore: criação de controllers authcontroller, categoria, produto e api.

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +12,10 @@ use Illuminate\Support\Facades\Route;
 //})->middleware('auth:sanctum');
 
 Route::apiResource('posts', PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+Route::post('/produto', [ProductController::class, 'product']);
+Route::post('/categoria', [CategoryController::class, 'category']);
